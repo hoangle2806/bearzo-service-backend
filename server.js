ff2f92903721a003e04ed852a37b5e5294969a14
hoangle2806@gmail.com <hoang@admins-MBP.lan>
2020-01-08 22:21:23 -0800

    set up routes for /api/users and /appi/auth

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// import routes
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+
 const app = express();
 
 // Connect Database
 connectDB();
 
 app.get('/', (req,res) => res.send('API running'))
+
+// Define routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 5000;
 
