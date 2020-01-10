8774b5914c59647f1f2812661cbdbe302070f3a8
hoangle2806@gmail.com <hoang@admins-MBP.lan>
2020-01-09 19:25:34 -0800

    add tokens for register user, expire in 360,000

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
@@ -41,10 +43,17 @@ async (req, res) => {
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-
-
-        // Return jwt
-        res.send('User Route');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+            if(err) throw err;
+            res.json({ token })
+        })
 
     } catch(err) {
         console.log(err.message);
