918490da52168d49e9636c450fd73cc86cd5c8d9
hoangle2806@gmail.com <hoang@admins-MBP.lan>
2020-01-09 19:55:10 -0800

    add auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 const router = express.Router();
 
-// @route       GET api.users
+// @route       GET api/auth
 // @des         Test Auth
 // @access      Public
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/', auth,
+    async (req, res) => {
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            res.json(user)
+        } catch(err) {
+            res.status(500).send('Internal Server Error')
+        }
+})
 
 module.exports = router;
