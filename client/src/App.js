d02967a79fa39c5e60d77d62516c2cdac3452b8d
hoangle2806@gmail.com <hoang@admins-MBP.lan>
2020-01-14 01:24:16 -0800

    added store reducers to our app

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,24 @@ import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './dashboard/components/dashboard';
 import { LoginComponent } from './login-form/components';
+// Redux stuff
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
-    <Fragment>
-      <Route exact path='/' component={LoginComponent} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/dashboard' component={Dashboard} />
-        </Switch>
-      </section>
-    </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+      <Fragment>
+        <Route exact path='/' component={LoginComponent} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Switch>
+        </section>
+      </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
